eac2d0dca440483569058f349076835b493c6ec2
xTirian <tirianx@yahoo.com>
2023-07-10 21:55:49 -0300

    primeiro commit do portifolio

---
+++ index.js
@@ -0,0 +1,9 @@
+var Emoji = window.document.getElementById("EmojiSan")
+
+function Piscar() {
+ Emoji.src = "./assets/images/piscadela.png"
+}
+
+function Sorrir(){
+ Emoji.src = "./assets/images/sorriso.png"
+}
